8f7977bf467c31299eeaa4e531a7fa67159c5c6c
anishbista92 <anishbista9235@gmail.com>
2023-12-28 18:27:07 +0545

    User Authentication done

--- online_bookstore/settings.py
+++ online_bookstore/settings.py
@@ -57,7 +57,7 @@ ROOT_URLCONF = "online_bookstore.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [BASE_DIR / "templates"],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
@@ -69,6 +69,9 @@ TEMPLATES = [
         },
     },
 ]
+
+LOGIN_REDIRECT_URL = "home"
+LOGOUT_REDIRECT_URL = "home"
 
 WSGI_APPLICATION = "online_bookstore.wsgi.application"
 
